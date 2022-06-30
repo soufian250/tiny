3ad35cadb231beb457a601826af5edd6be15869e
Soufian <selhabib@ze-camping.fr>
2022-06-30 08:44:15 +0100

    Add php function for compressing images

--- src/Controller/PuppyController.php
+++ src/Controller/PuppyController.php
@@ -3,9 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Puppy;
+use App\Form\ExcelFormatType;
 use App\Form\PuppyType;
 use App\Repository\PuppyRepository;
 use Exception;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,6 +16,11 @@ use Tinify\Tinify;
 use PhpOffice\PhpSpreadsheet\Reader\Csv as ReaderCsv;
 use PhpOffice\PhpSpreadsheet\Reader\Ods as ReaderOds;
 use PhpOffice\PhpSpreadsheet\Reader\Xlsx as ReaderXlsx;
+use PhpOffice\PhpSpreadsheet\Writer\Csv;
+use PhpOffice\PhpSpreadsheet\Writer\Ods;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+use Symfony\Component\HttpFoundation\StreamedResponse;
+
 
 
 /**
@@ -81,9 +88,111 @@ class PuppyController extends AbstractController
 
         $executionEndTime = microtime(true);
         $seconds = $executionEndTime - $executionStartTime;
-         return new Response("Images downloaded successfully in $seconds");
+        return new Response("Images downloaded successfully in $seconds");
 
     }
+
+    /**
+     * @Route("/export", name="export")
+     */
+    public function exportAction()
+        {
+
+            $format = 'xlsx';
+            $filename = 'Browser_characteristics.'.$format;
+
+            $spreadsheet = $this->createSpreadsheet();
+
+
+            $contentType = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet';
+            $writer = new Xlsx($spreadsheet);
+
+            $response = new StreamedResponse();
+            $response->headers->set('Content-Type', $contentType);
+            $response->headers->set('Content-Disposition', 'attachment;filename="'.$filename.'"');
+            $response->setPrivate();
+            $response->headers->addCacheControlDirective('no-cache', true);
+            $response->headers->addCacheControlDirective('must-revalidate', true);
+            $response->setCallback(function() use ($writer) {
+                $writer->save('php://output');
+            });
+
+
+            return $response;
+
+        }
+
+    /**
+     * @Route("/compresstest", name="export")
+     */
+    public function testAction()
+        {
+
+
+            $destination_url = $this->getParameter('images_directory');
+            $source_img = $this->getParameter('images_directory_source');
+
+
+            $this->compressImage($source_img, $destination_url, 30);
+            $error = "Image Compressed successfully";
+
+            dump($error);die;
+
+
+            return new Response("Images downloaded successfully");
+
+        }
+
+
+    public function createSpreadsheet()
+    {
+
+
+        $spreadsheet = new Spreadsheet();
+        // Get active sheet - it is also possible to retrieve a specific sheet
+        $sheet = $spreadsheet->getActiveSheet();
+
+        // Set cell name and merge cells
+
+
+        // Set column names
+        $columnNames = [
+            'Type',
+            'Source',
+            'Path',
+            'Size(bytes)',
+        ];
+        $columnLetter = 'A';
+        foreach ($columnNames as $columnName) {
+            // Allow to access AA column if needed and more
+            $sheet->setCellValue($columnLetter.'1', $columnName);
+            $columnLetter++;
+        }
+
+        // Add data for each column
+        $columnValues = [
+            ['Google Chrome', 'Google Inc.', 'September 2, 2008', 'C++'],
+            ['Firefox', 'Mozilla Foundation', 'September 23, 2002', 'C++, JavaScript, C, HTML, Rust'],
+            ['Microsoft Edge', 'Microsoft', 'July 29, 2015', 'C++'],
+            ['Safari', 'Apple', 'January 7, 2003', 'C++, Objective-C'],
+            ['Opera', 'Opera Software', '1994', 'C++'],
+            ['Maxthon', 'Maxthon International Ltd', 'July 23, 2007', 'C++'],
+            ['Flock', 'Flock Inc.', '2005', 'C++, XML, XBL, JavaScript'],
+        ];
+
+        $i = 2; // Beginning row for active sheet
+        foreach ($columnValues as $columnValue) {
+            $columnLetter = 'A';
+            foreach ($columnValue as $value) {
+                $sheet->setCellValue($columnLetter.$i, $value);
+                $columnLetter++;
+            }
+            $i++;
+        }
+
+        return $spreadsheet;
+    }
+
 
 
     protected function readFile($filename)
@@ -137,6 +246,26 @@ class PuppyController extends AbstractController
 
             return $data;
         }
+    }
+
+    private function compressImage( $source, $destination, $quality)
+    {
+        $info = getimagesize($source);
+
+        if ($info['mime'] == 'image/jpeg')
+            $image = imagecreatefromjpeg($source);
+
+        elseif ($info['mime'] == 'image/gif')
+            $image = imagecreatefromgif($source);
+
+        elseif ($info['mime'] == 'image/png')
+            $image = imagecreatefrompng($source);
+
+        if(isset($image))
+            imagejpeg($image, $destination, 50);
+
+        return $destination;
+
     }
 
 
